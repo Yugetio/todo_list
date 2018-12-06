cf34a488ed08251a3a5ff5e368f8c291e2e70851
Vladyslav Kaminskyi <vkaminsky@devtoday.com>
2018-12-06 14:39:38 +0200

    added db config and entity...

---
+++ src/Entity/TodoList.php
@@ -0,0 +1,76 @@
+<?php
+# php bin/console doctrine:migrations:diff
+# php bin/console doctrine:migrations:migrate
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TodoListRepository")
+ */
+class TodoList
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $ready;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $user_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getReady(): ?bool
+    {
+        return $this->ready;
+    }
+
+    public function setReady(bool $ready): self
+    {
+        $this->ready = $ready;
+
+        return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
